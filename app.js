7401f32cad74b2be06b4c28ba61d0b671978745f
Jonathon Shields <jonathonshields@gmail.com>
2016-12-06 16:28:07 +1300

    fiddlin with colours

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ var ratio;
 
 var shapes = ['square', 'circle', 'triangle'];
 
+var colourPalette = ['#FFFFFF','#F4FFFF','#F8FFFF','#FBFFFF', '#EFFFFF']
+
 function init(){
     canvas = document.getElementById('app');
 	ctx = canvas.getContext('2d');
     resizeCanvas();
-    ctx.fillStyle='#CCEEFF';
+    ctx.fillStyle='#BFEFFF';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ratio = canvas.width/1100;
+    if(canvas.height > canvas.width){
+        ratio = canvas.height/1200;
+    } else {
+        ratio = canvas.width/1200;
+    }
     console.log(canvas.width);
     buildSnowflake(Math.random());
 }
@@ -31,18 +37,20 @@ function drawShapes(shape){
     var xOffset = (Math.random()*300) * ratio;
     var yOffset = 0;
     var size = (Math.random()*100 + 20) * ratio;
+    shuffle(colourPalette);
+    var colourToUse = colourPalette[0];
     for(var i = 0; i < 6; i++){
         if(shape == 'square'){ 
-            drawRotatedRect(-size/2 + xOffset,-size/2 + yOffset,size,size,i*60);
+            drawRotatedRect(-size/2 + xOffset,-size/2 + yOffset,size,size,i*60, colourToUse);
         } else if(shape == 'circle'){
-            drawCircle(xOffset, yOffset, size/5, i*60);
+            drawCircle(xOffset, yOffset, size/5, i*60, colourToUse);
         } else if(shape == 'triangle'){
-            drawRotatedTri(xOffset, yOffset, size, i*60);
+            drawRotatedTri(xOffset, yOffset, size, i*60, colourToUse);
         }
     }
 }
 
-function drawRotatedRect(x,y,width,height,degrees){
+function drawRotatedRect(x,y,width,height,degrees, colour){
 
     // first save the untranslated/unrotated context
     ctx.save();
@@ -58,7 +66,7 @@ function drawRotatedRect(x,y,width,height,degrees){
     //       so the rect needs to be offset accordingly when drawn
     ctx.rect(x, y, width,height);
 
-    ctx.fillStyle = "cornflowerblue";
+    ctx.fillStyle = colour;
     ctx.fill();
 
     // restore the context to its untranslated/unrotated state
@@ -66,7 +74,7 @@ function drawRotatedRect(x,y,width,height,degrees){
 
 }
 
-function drawRotatedTri(x, y, size, degrees){
+function drawRotatedTri(x, y, size, degrees, colour){
     ctx.save();
     ctx.beginPath();
     ctx.translate( canvas.width/2, canvas.height/2 );
@@ -75,13 +83,12 @@ function drawRotatedTri(x, y, size, degrees){
     
     ctx.moveTo(x-0.5*size,y);
     ctx.lineTo(x+0.5*size,y+0.5*size);
-    ctx.lineTo(x+0.5*size,y-0.5*size);
-    ctx.fillStyle = "cornflowerblue";
+    ctx.fillStyle = colour;
     ctx.fill();
     ctx.restore();
 }
 
-function drawCircle(x,y, radius, degrees){
+function drawCircle(x,y, radius, degrees, colour){
     ctx.save();
     
     ctx.beginPath();
@@ -89,7 +96,7 @@ function drawCircle(x,y, radius, degrees){
     ctx.rotate(degrees*Math.PI/180);
 
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "cornflowerblue";
+    ctx.fillStyle = colour;
     ctx.fill();
     ctx.restore();
 }
